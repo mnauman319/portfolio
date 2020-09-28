796736788339a92ec8f863de56ec9dcc830e8e6b
mnauman319 <mnauman319@gmail.com>
2020-09-28 13:01:21 -0500

    changed styling to show screenshot of application then user can click on demo to visit the app or click source code to go to github.

--- src/app/components/applications/applications.component.ts
+++ src/app/components/applications/applications.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./applications.component.scss']
 })
 export class ApplicationsComponent implements OnInit {
-  
+  showDetails:Map<string,boolean> = new Map();
   constructor() { }
 
   ngOnInit(): void {
+    this.showDetails.set("snapgram",false);
+    this.showDetails.set("reimbursement",false);
+    this.showDetails.set("bank",false);
+  }
+
+  showImgDetail(event){
+    let divName:string = event.target.id + "-container";
+    let div = document.getElementById(divName);
+    div.style.visibility = (this.showDetails.get(event.target.name)) ?'hidden' : 'visible';
+    this.showDetails.set(event.target.name,!this.showDetails.get(event.target.name));
   }
 
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,14 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor() { }
-  visibiltyMap:Map<string,boolean> = new Map;
-  ngOnInit(): void {
 
-    this.visibiltyMap.set("github",true);
-    this.visibiltyMap.set("linkedin",false);
-    this.visibiltyMap.set("resume",false);
-    this.visibiltyMap.set("codewars",false);
-    this.visibiltyMap.set("applications",false);
+  ngOnInit(): void {
   }
 
 }
